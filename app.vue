39ef54f6d3722d8d20726b9f09e811920ae2dc32
beks <bekzat.bagdatuly03@gmail.com>
2024-04-16 19:57:44 +0500

    project structure

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <nuxt-page/>
   </div>
 </template>
---
+++ components/Welcome.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex flex-col items-center justify-center h-screen bg-gray-100">
+    <img src="https://seeklogo.com/images/N/nuxt-logo-D5098E139E-seeklogo.com.png" alt="Nuxt 3 Logo" class=" mb-8">
+    <h1 class="text-3xl font-bold mb-4">Welcome to Nuxt 3 with Tailwind CSS</h1>
+    <p class="text-lg text-gray-600 mb-8">Get started by editing
+      <code class="px-2 py-1 bg-gray-200 rounded-md">pages/index.vue</code>
+    </p>
+    <a href="https://github.com/roenbis" class="px-4 py-2 bg-emerald-500 text-white rounded-md hover:bg-emerald-600 transition duration-300">
+      Enjoy!
+    </a>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
